b381261ea6a9f60096ea529da571a4e23f099484
anilkaracay <anil.karacay06@hotmail.com>
2022-09-17 10:42:02 +0300

    comment lines deleted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,4 @@
-'use strict';
-
-
-
-/**
- * add Event on elements
- */
+"use strict";
 
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
@@ -14,13 +8,7 @@ const addEventOnElem = function (elem, type, callback) {
   } else {
     elem.addEventListener(type, callback);
   }
-}
-
-
-
-/**
- * navbar toggle
- */
+};
 
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
@@ -30,22 +18,16 @@ const overlay = document.querySelector("[data-overlay]");
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
 const closeNavbar = function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
-}
+};
 
 addEventOnElem(navbarLinks, "click", closeNavbar);
-
-
-
-/**
- * header & back top btn show when scroll down to 100px
- */
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
@@ -58,6 +40,6 @@ const headerActive = function () {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
-}
+};
 
 addEventOnElem(window, "scroll", headerActive);
